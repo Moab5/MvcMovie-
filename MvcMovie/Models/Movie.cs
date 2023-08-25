96aca1de6648dc79d9aaa11653a393691af7d7a1
Mohammed <Moabadejumat@gmail.com>
2023-08-25 15:23:22 +0400

    Add a new field (Rating)

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -16,5 +16,9 @@ namespace MvcMovie.Models
 
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+        public string? Rating
+        {
+            get; set;
+        }
     }
 }
